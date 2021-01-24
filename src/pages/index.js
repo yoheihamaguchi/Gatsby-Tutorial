55ca8d240488a2a6419f37681fe5cba8451e70af
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-01-24 12:09:23 +0900

    createing nested layout components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Layout from '../components/layout'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>
+          What do I like to do? Lots of course but definitely enjoy building
+          websites.
+      </p>
+      </div>
+    </Layout>
+  )
 }
